2d6f30db137f13268548e00b927e9653d7df8ce8
sknithin06 <nithinsk939@gmail.com>
2021-07-04 02:14:14 +0530

    number is generated

--- powernum.java
+++ powernum.java
@@ -1,7 +1,16 @@
 package Java;
-
+import java.util.Scanner;
 public class powernum {
 	public static void main(String[] args) {
 		System.out.println("Check The Power of 2");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a value of a:"); 
+		int n = sc.nextInt();
+		double x = 0;
+		for (int i = 0; i <= n; i++) {		
+			x = Math.pow(2,i);
+		}
+		System.out.println("Power of the Given Number is:" +x);	
 	}
 }
